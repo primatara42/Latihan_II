8da981f74277e02cafd6e780326f16a32dbe206c
rickyputrasardika22 <primatara42@gmail.com>
2022-11-07 21:48:36 +0700

    #8 Pengukur Bahan Bakar

--- src/com/latihanii/Car.java
+++ src/com/latihanii/Car.java
@@ -5,6 +5,7 @@ public class Car
     private String name;
     private String color;
     private int distance = 0;
+    private int fuel = 100;
 
     Car(String name, String color)
     {
@@ -22,11 +23,22 @@ public class Car
         return this.color;
     }
 
+    public int getDistance()
+    {
+        return this.distance;
+    }
+
+    public int getFuel()
+    {
+        return this.fuel;
+    }
+
     public void printData()
     {
         System.out.println("Nama: " + getName());
         System.out.println("Warna: " + getColor());
-        System.out.println("Jarak: " + this.distance + "km");
+        System.out.println("Jarak: " + getDistance() + "km");
+        System.out.println("Bahan Bakar: " + getFuel() + "L");
     }
 
 }
